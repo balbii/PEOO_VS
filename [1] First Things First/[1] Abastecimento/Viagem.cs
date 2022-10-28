e4a9acaf8f3ed9816ea9467179c9280ff82f7e8b
Renan Balbino de Medeiros <renan.medeiros@academico.ifrn.edu.br>
2022-10-28 19:03:44 -0300

    Abastecimento App

---
+++ [1] First Things First/[1] Abastecimento/Viagem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1__Abastecimento
+{
+    class Viagem
+    {
+        private double valorLitro, valorPago, kmRodados;
+        
+        public void SetValorLitro(double v)
+        {
+            if (v > 0) valorLitro = v;
+        }
+        public void SetValorPago(double v)
+        {
+            if (v > 0) valorPago = v;
+        }
+        public void SetKmRodados(double v)
+        {
+            if (v > 0) kmRodados = v;
+        }
+        public double MediaKmLitro()
+        {
+            return kmRodados / (valorPago / valorLitro);
+        }
+        public double MediaReaisKm()
+        {
+            return valorPago / kmRodados;
+        }
+    }
+}
