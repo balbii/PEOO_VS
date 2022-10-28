4e8255ad15b2273be62548d6a195809b245a32b4
Renan Balbino de Medeiros <renan.medeiros@academico.ifrn.edu.br>
2022-10-28 18:24:42 -0300

    EquacaoIIGrau App

---
+++ [1] First Things First/Equacao2grau/Equacao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equacao2grau
+{
+    class Equacao
+    {
+        private double a, b, c;
+        public void SetABC(double a, double b, double c)
+        {
+            if (a != 0) { this.a = a; this.b = b; this.c = c; }
+        }
+        public void GetABC(out double a, out double b, out double c)
+        {
+            a = this.a; b = this.b; c = this.c;
+        }
+        public double Delta()
+        {
+            return b * b - 4 * a * c;
+        }
+        public bool RaizesReais()
+        {
+            if (Delta() >= 0) return true;
+            else return false;
+        }
+        public bool X1(out double x)
+        {
+            x = (-b + Math.Sqrt(Delta())) / 2 * a;
+            if (RaizesReais()) return true;
+            else return false;
+        }
+        public bool X2(out double x)
+        {
+            x = (-b - Math.Sqrt(Delta())) / 2 * a;
+            if (RaizesReais()) return true;
+            else return false;
+        }
+    }
+}
---
+++ [1] First Things First/Equacao2grau/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Equacao2grau
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = double.Parse(txtC.Text);
+
+            Equacao eq = new Equacao();
+            eq.SetABC(a, b, c);
+            txtDelta.Text = eq.Delta().ToString();
+            double x1, x2;
+            if (eq.X1(out x1)) txtX1.Text = x1.ToString();
+            if (eq.X2(out x2)) txtX2.Text = x2.ToString();
+        }
+    }
+}
