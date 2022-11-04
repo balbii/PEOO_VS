bd2f2cca4c4f10e134ed9138d3c2f24a2ce8d351
Renan Balbino de Medeiros <renan.medeiros@academico.ifrn.edu.br>
2022-11-04 15:55:57 -0300

    Bingo app em progresso

--- [1] First Things First/[1] Abastecimento/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ [1] First Things First/[1] Abastecimento/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace _1__Abastecimento {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/[1] Abastecimento;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/[1] Abastecimento;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
