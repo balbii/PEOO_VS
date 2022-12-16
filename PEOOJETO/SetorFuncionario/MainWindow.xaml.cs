ec266db52e6d894e810edfd1c24c5643d484d5ba
Renan Balbino de Medeiros <renan.medeiros@academico.ifrn.edu.br>
2022-12-16 09:03:43 -0300

    projeto wpf

---
+++ PEOOJETO/SetorFuncionario/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SetorFuncionario
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Setor set;
+        private void criarsetor_Click(object sender, RoutedEventArgs e)
+        {
+            set = new Setor(nomesetor.Text, siglasetor.Text);
+            adicionarfuncionario.IsEnabled = true;
+            lista.Items.Clear();
+        }
+        private void adicionarfuncionario_Click(object sender, RoutedEventArgs e)
+        {
+            Funcionario f1 = new Funcionario(nomeFuncionario.Text, matriculafuncionario.Text, emailfuncionario.Text, double.Parse(salariofuncionario.Text));
+            set.Inserir(f1);
+            lista.Items.Add(f1);
+            nomeFuncionario.Text = "";
+            matriculafuncionario.Text = "";
+            emailfuncionario.Text = "";
+            salariofuncionario.Text = "";
+        }
+
+        private void btnMedia_Click(object sender, RoutedEventArgs e)
+        {
+            mostrarMedia.Text = "R$" + set.MediaSalario().ToString("0.00");
+        }
+    }
+}
---
+++ PEOOJETO/SetorFuncionario/Setor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetorFuncionario
+{
+    class Funcionario
+    {
+        private string nome, matricula, email;
+        private double salario;
+        public Funcionario(string nome, string matricula, string email, double salario)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+            this.email = email;
+            this.salario = salario;
+        }
+        public double GetSalario()
+        {
+            return salario;
+        }
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {matricula}\nSalário: R${salario}";
+        }
+    }
+    class Setor
+    {
+        private string nome, sigla;
+        private Funcionario[] funcionarios = new Funcionario[1];
+        int k = 0;
+        public Setor(string nome, string sigla)
+        {
+            this.nome = nome;
+            this.sigla = sigla;
+        }
+        public void Inserir(Funcionario f)
+        {
+            if (funcionarios.Length == k)
+            {
+                Array.Resize(ref funcionarios, funcionarios.Length + 1);
+            }
+            funcionarios[k] = f;
+            k++;
+        }
+        public Funcionario[] Listar()
+        {
+            Funcionario[] lista = new Funcionario[k];
+            Array.Copy(funcionarios, lista, k);
+            return lista;
+        }
+        public double MediaSalario()
+        {
+            double total = 0;
+            foreach (Funcionario f in funcionarios)
+            {
+                total += f.GetSalario();
+            }
+       
+            return total / k;
+        }
+        public override string ToString()
+        {
+            return $"{sigla} - {nome}";
+        }
+    }
+}
